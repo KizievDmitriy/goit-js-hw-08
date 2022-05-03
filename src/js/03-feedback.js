8c5c302f60009870c9283e1eb4eb059a370734ff
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-03 11:47:58 +0300

    done
    Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('[name="email"]');
+const textAreaRef = document.querySelector('[name="message"]');
+
+onResetPage();
+
+formRef.addEventListener('input', throttle(onInputHandler, 500));
+
+function onInputHandler(e) {
+  let currentLocalStorage = localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: inputRef.value, message: textAreaRef.value }),
+  );
+  currentLocalStorage ? JSON.parse(localStorage.getItem('feedback-form-state')) : {};
+}
